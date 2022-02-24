40729be88ada54004475b2d3a2c0d9f217715adc
Kacper <kacper916@gmail.com>
2022-02-24 23:12:20 +0100

    lyrics of first found artist song printed

--- songitionary.py
+++ songitionary.py
@@ -1,4 +1,6 @@
 from rauth import OAuth2Service, OAuth2Session
+from bs4 import BeautifulSoup
+import re
 import json
 
 def getQueryParams(queryPhrase):
@@ -19,9 +21,10 @@ def getArtistId(session, queryPhrase):
 def getAllSongAddresses(session, artistId):
 
     songAddresses = set()
-    next_page=1;
+    next_page=1
 
-    while next_page != None:  
+    while next_page != None:
+        print("*",end='')  
         uri = "https://api.genius.com/artists/{0}/songs?per_page=50&page={1}".format(artistId,next_page)
         response = session.request("get", uri)
         parsedResponse = response.json()
@@ -29,10 +32,38 @@ def getAllSongAddresses(session, artistId):
         next_page = parsedResponse["response"]["next_page"]
         for song in songs:
             songAddresses.add(song["path"])
+    print("")
+    print("Znaleziono {0} piosenek.".format(len(songAddresses)))
     return songAddresses
 
-session = initializeSession("ENTER BEARER")
+def getSongLyrics(session, path):
+    uri = "https://genius.com{0}".format(path)
+    page = session.request("get", uri)
+    html = BeautifulSoup(page.text, "html.parser")
+
+    for br in html.find_all("br"):
+        br.replace_with("\n")
+
+    lyricsParts = html.find_all("div",{"data-lyrics-container":True})
+    lyrics = ""
+    for part in lyricsParts:
+        text = part.get_text()
+        text = re.sub("\[( *(\w|&|:|-)* *)*\]", "", text)
+        text = text.replace("(","")
+        text = text.replace(")","")
+        text = text.replace(",","")
+        text = text.replace(".","")
+        text = text.replace("?","")
+        text = text.replace("!","")
+        text = text.lower()
+        lyrics += text
+    return lyrics
+
+
+session = initializeSession("BEARER TOKEN")
 name = input("PODAJ WYKONAWCE: ")
 id = getArtistId(session, name)
 print("Jego Id to {0}".format(id))
 addresses = getAllSongAddresses(session, id)
+lyrics = getSongLyrics(session,addresses.pop())
+print(lyrics)
